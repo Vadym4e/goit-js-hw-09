8f8a84e9fe890a727c6413ac6aee24d50709c320
Vadym4e <vadym.o92@gmail.com>
2023-03-05 18:07:01 +0200

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
 
+let intervalId = null;
+
+startButton.addEventListener('click', startBackgroundColorChange);
+stopButton.addEventListener('click', stopBackgroundColorChange);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBackgroundColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function startBackgroundColorChange() {
+  startButton.disabled = true;
+  intervalId = setInterval(changeBackgroundColor, 1000);
+}
+
+function stopBackgroundColorChange() {
+  startButton.disabled = false;
+  clearInterval(intervalId);
+}
