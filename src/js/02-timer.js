9735288b717bf57fd7fd8a8c6785caba2abf396a
Vadym4e <vadym.o92@gmail.com>
2023-03-11 14:07:49 +0200

    task-02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,10 +1,13 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
 const flatpickrInput = document.querySelector('#datetime-picker');
 const startButton = document.querySelector('[data-start]');
-
-const currentTime = Date.now();
+const days = document.querySelector('span[data-days]');
+const hours = document.querySelector('span[data-hours]');
+const minutes = document.querySelector('span[data-minutes]');
+const seconds = document.querySelector('span[data-seconds]');
 
 startButton.disabled = true;
 
@@ -14,18 +17,51 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0].getTime());
-    selectedTime.time = selectedDates.getTime();
     if (selectedDates[0] < options.defaultDate) {
-      window.alert('Please choose a date in the future');
+      Notiflix.Notify.failure('Please choose a date in the future');
       return;
     }
-    // console.log('currentTime', currentTime);
+
     startButton.disabled = false;
   },
 };
 
 flatpickr(flatpickrInput, options);
 
-console.log('currentTime', currentTime);
-// console.log('selectedTime', selectedTime.time);
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+
+  const hours = Math.floor((ms % day) / hour);
+
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+addLeadingZero = value => value.toString().padStart(2, '0');
+
+startButton.addEventListener('click', () => {
+  let timerStart = setInterval(() => {
+    let countdown = new Date(flatpickrInput.value) - new Date();
+    // console.log(countdown);
+    startButton.disabled = true;
+
+    if (countdown >= 0) {
+      let convertTime = convertMs(countdown);
+
+      days.textContent = addLeadingZero(convertTime.days);
+      hours.textContent = addLeadingZero(convertTime.hours);
+      minutes.textContent = addLeadingZero(convertTime.minutes);
+      seconds.textContent = addLeadingZero(convertTime.seconds);
+    } else {
+      clearInterval(timerStart);
+    }
+  }, 1000);
+});
