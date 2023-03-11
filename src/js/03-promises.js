d1a4de6ebb3623547a3062b0d22bb31aeddff2e9
Vadym4e <vadym.o92@gmail.com>
2023-03-11 17:01:37 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,52 +1,7 @@
-// import Notiflix from 'notiflix';
-
-// const form = document.querySelector('.form');
-// const delay = document.querySelector('[name="delay"]');
-// const step = document.querySelector('[name="step"]');
-// const amount = document.querySelector('[name="amount"]');
-// const buttonCreate = document.querySelector('[type="submit"]');
-
-// function createPromise(position, delay) {
-//   const promise = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       const shouldResolve = Math.random() > 0.3;
-
-//       if (shouldResolve) {
-//         resolve({ position, delay });
-//       } else {
-//         reject({ position, delay });
-//       }
-//     });
-//   });
-
-//   return promise;
-// }
-
-// buttonCreate.addEventListener('click', evt => {
-//   let firstDalay = Number(delay.value);
-//   let stepDelay = Number(step.value);
-
-//   for (let i = 0; i < amount.value; i += 1) {
-//     createPromise(i + 1, firstDalay + 1 * stepDelay)
-//       .then(({ position, delay }) => {
-//         Notiflix.Notify.success(
-//           `✅ Fulfilled promise ${position} in ${delay}ms`
-//         );
-//       })
-
-//       .catch(({ position, delay }) => {
-//         Notiflix.Notify.success(
-//           `❌ Rejected promise ${position} in ${delay}ms`
-//         );
-//       });
-//   }
-
-//   form.reset();
-// });
-
 import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.6.min.css';
 
+const form = document.querySelector('.form');
 const delay = document.querySelector('input[name="delay"]');
 const step = document.querySelector('input[name="step"]');
 const amount = document.querySelector('input[name="amount"]');
